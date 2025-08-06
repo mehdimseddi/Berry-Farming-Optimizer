c4c6428d2a9454c8c3309daf292f9d6673dc0f7a
C00LiMoUn <mehdimseddi306@gmail.com>
2025-08-06 13:34:14 +0100

    frontend fixed

--- frontend/src/components/OptimizationResults.tsx
+++ frontend/src/components/OptimizationResults.tsx
@@ -1,6 +1,6 @@
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { api } from "@/lib/api";
 import { toast } from "sonner";
 
